6e01e9989ceb9fc205d17e5246083fc8fba7a680
Bishwas Pandey <pandeybishwas5@gmail.com>
2024-05-21 13:36:50 +1000

    fix issues

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
-
-
-
+// Load environment variables from .env file
+dotenv.config();
 
 const options = {
   method: 'GET',
@@ -22,7 +22,7 @@ const options = {
     page: '1'
   },
   headers: {
-    'X-RapidAPI-Key': 'dbd62d8321mshbd058676ead6c8ep155606jsn5cfd448fa71e',
+    'X-RapidAPI-Key': process.env.API_KEY,
     'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
   }
 };
@@ -31,72 +31,75 @@ const options = {
 const app = express();
 const port = 5000;
 
-
-
 // 3. Use the public folder for static files.
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.json()); // Add this line for handling JSON payloads
 
 app.get("/", (req, res) => {
-  res.render("index.ejs")
+  res.render("index.ejs");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about.ejs")
+  res.render("about.ejs");
 });
 
 app.get("/search", (req, res) => {
-  res.render("search.ejs")
+  res.render("search.ejs");
+});
+
+app.post("/search", async (req, res) => {
+  try {
+    const mediaType = req.body.type;
+    if (!mediaType) {
+      return res.status(400).json({ error: "Media type is required" });
+    }
+    const genreList = ['action', 'comedy', 'thriller', 'horror'];
+    const randomGenre = genreList[Math.floor(Math.random() * genreList.length)];
+    console.log("Random Genre:", randomGenre);
+
+    const updatedOptions = {
+      ...options,
+      params: {
+        ...options.params,
+        genre: randomGenre,
+        type: mediaType,
+      },
+    };
+
+    const response = await axios.request(updatedOptions);
+    console.log("API Response:", response.data);
+    const apiResponse = response.data;
+    const movies = apiResponse.results;
+
+    res.render("index.ejs", { movies });
+  } catch (error) {
+    console.error("Error in /search route:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
 
-app.post("/search", async(req,res) => {
-  const mediaType = req.body.type;
-  const genreList = ['action', 'comedy', 'thriller', 'horror'];
-  const randomGenre = genreList[Math.floor(Math.random() * genreList.length)];
-  console.log(randomGenre);
-
-  const updatedOptions = {
-    ...options,
-    params: {
-      genre: randomGenre,
-      type: mediaType,
-    },
-  };
-  try {
-    const response = await axios.request(updatedOptions);
-    console.log(response);
-    const apiResponse = response.data;
-    console.log(apiResponse);
-    const movies = apiResponse.results;
-    res.render("index.ejs", {movies});
-    
-  } catch (error) {
-    res.status(500).json({ error: "An error occurred" });
-  }
-})
-
-
-app.post("/searchtitle", async(req,res) => {
+app.post("/searchtitle", async (req, res) => {
   const newTitle = req.body.type;
   console.log(newTitle);
-  const options = {
+  const titleOptions = {
     method: 'GET',
     url: `https://cinema-api.p.rapidapi.com/get_ids/${newTitle}/movies`,
     headers: {
-      'X-RapidAPI-Key': 'dbd62d8321mshbd058676ead6c8ep155606jsn5cfd448fa71e',
+      'X-RapidAPI-Key': process.env.API_KEY,
       'X-RapidAPI-Host': 'cinema-api.p.rapidapi.com'
     }
   };
-  
+
   try {
-    const response = await axios.request(options);
+    const response = await axios.request(titleOptions);
     const apiResponse = response.data;
     const results = apiResponse.data;
-    res.render("search.ejs", {results});
+    res.render("search.ejs", { results });
   } catch (error) {
     console.error(error);
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
@@ -108,8 +111,9 @@ const platformData = {
   tubitv: '/images/tubi.png',
   amazonprime: '/images/prime.png',
   appletvplus: '/images/apple.png'
-}
-app.get("/movie/:id", async(req, res) => {
+};
+
+app.get("/movie/:id", async (req, res) => {
   const movieId = req.params.id;
   const updatedOptions = {
     ...options,
@@ -118,19 +122,18 @@ app.get("/movie/:id", async(req, res) => {
       imdbid: movieId,
     },
   };
+
   try {
     const response = await axios.request(updatedOptions);
     const movieDetails = response.data;
     const streaming = movieDetails.streamingAvailability.country.US;
-    res.render("movie.ejs", {movieDetails, streaming, platformData});
+    res.render("movie.ejs", { movieDetails, streaming, platformData });
   } catch (error) {
-    res.status(500).json({error: "An error occured" });
-    
+    res.status(500).json({ error: "An error occurred" });
   }
 });
 
-
 // 6. Listen on your predefined port and start the server.
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  console.log(`Server is running on port ${port}`);
+});
